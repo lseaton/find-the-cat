d42bc3a7e0080069dd7bd696d09bb3afbb1f4f63
lseaton <73501741+lseaton@users.noreply.github.com>
2025-02-26 12:25:10 -0800

    Implement logic for hovering over tiles and add background image

--- README.md
+++ README.md
@@ -8,5 +8,7 @@ A pixel-drawing program with a twist.
 <h3>Screenshot:</h3>
 <h3>Tools:</h3>
 <ul>Visual Studio Code, Git, Github, Pixabay, Flaticon.</ul>
-<h3>Credits (Pixabay):</h3>
+<h3>Credits:</h3>
 <ul>Color wheel from <a href="https://pixabay.com/users/thedigitalartist-202249/">TheDigitalArtist</a></ul>
+<ul>Background galaxy rainbow from <a href="https://pixabay.com/users/geralt-9301/">geralt</a></ul>
+<ul>Mini raindow icon from <a href="https://www.flaticon.com/authors/freepik">Freepik</a></ul>
--- sketch.js
+++ sketch.js
@@ -1,14 +1,21 @@
 //Scratch pad - everything starts black, reveals background (popsocket-style) image as you go. You can scratch it all off or make rainbow drawings
-let gridSize = 36;
+let gridSize = 35;
 function populateGrid(size) {
 	for (let i = 0; i < size * size; i++) {
 		//will be 256 instead of 50 for 16x16 grid
 		let d = document.createElement("div");
 		d.className = "tile";
+		d.id = "tile" + i.toString(); //unique ID
 		d.style.width = 560 / size.toString() + "px";
 		d.style.height = 560 / size.toString() + "px";
+		d.addEventListener("mouseover", revealColor);
 		document.getElementById("grid").appendChild(d);
 	}
+}
+
+function revealColor(e) {
+	let tile = document.getElementById(e.target.id);
+	tile.style.background = "transparent";
 }
 
 populateGrid(gridSize);
